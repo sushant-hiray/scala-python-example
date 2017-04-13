cda1ccea0c1d3781644ab010b2e02a9cc5b5cd99
David Portabella <david.portabella@gmail.com>
2017-04-13 14:18:00 +0000

    fix on python script dir, and cast from Int to Long

--- src/main/scala/com/sushant/example/JepAddExample.scala
+++ src/main/scala/com/sushant/example/JepAddExample.scala
@@ -4,14 +4,14 @@ import jep.Jep
 
 object JepAddExample extends App {
   val jep = new Jep()
-  jep.runScript("add.py")
+  jep.runScript("src/main/python/add.py")
   val a = 2
   val b = 3
   // There are multiple ways to evaluate. Let us demonstrate them:
   jep.eval(s"c = add($a, $b)")
-  val ans = jep.getValue("c").asInstanceOf[Int]
+  val ans = jep.getValue("c").asInstanceOf[Long]
   println(ans)
-  val ans2 = jep.invoke("add", a.asInstanceOf[AnyRef], b.asInstanceOf[AnyRef]).asInstanceOf[Int]
+  val ans2 = jep.invoke("add", a.asInstanceOf[AnyRef], b.asInstanceOf[AnyRef]).asInstanceOf[Long]
   println(ans2)
 
 }
